8686a9d9e75f13da1a614de1f51413b020bc66c2
bissie <5p4mail@bissinger-online.de>
2021-06-17 08:13:09 +0200

    PSEC-61: Integrate new API für dealer gateway. Load dealer state for order and display in easy credit tab

---
+++ Application/Controller/Admin/EasyCreditOrderOverviewController.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace OxidProfessionalServices\EasyCredit\Application\Controller\Admin;
+
+
+class EasyCreditOrderOverviewController extends EasyCreditOrderOverviewController_parent
+{
+
+}
