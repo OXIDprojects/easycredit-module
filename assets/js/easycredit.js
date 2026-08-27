a93f1d074a20141453f2a0209927d3f50b1e2bf7
evollrath <96vlocky96@gmail.com>
2026-08-27 14:50:04 +0200

    0130343 fix JS error

--- assets/js/easycredit.js
+++ assets/js/easycredit.js
@@ -36,11 +36,15 @@ function oscHandlePaymentRadioChange(e) {
     let elemInstallmentContainer = document.getElementById('easycredit_installment_container');
     let elemInvoiceContainer = document.getElementById('easycredit_invoice_container');
     if (e.target.id === "payment_easycreditinstallment" && e.target.checked === true) {
-        oscApexAddEvents();
-        elemInstallmentContainer.style.display = 'block';
+        if (elemInstallmentContainer) {
+            oscApexAddEvents();
+            elemInstallmentContainer.style.display = 'block';
+        }
     } else {
-        oscApexRevertEvents();
-        elemInstallmentContainer.style.display = 'none';
+        if (elemInstallmentContainer) {
+            oscApexRevertEvents();
+            elemInstallmentContainer.style.display = 'none';
+        }
     }
 
     if (e.target.id === "payment_easycreditinvoice" && e.target.checked === true) {
@@ -98,16 +102,15 @@ function oscGetApexNextButton() {
 function oscHandlePaymentForm(e) {
     var paymentForm = document.getElementById('payment');
 
-    if (paymentForm.elements['payment_easycreditinstallment'].checked === true) {
-        if (document.getElementById("easycredit_installment_agreement_error")) {
-            document.getElementById("easycredit_installment_agreement_error").style.display = "none";
-            if (document.getElementById("easycredit_installment_agreement").checked === false) {
-                e.preventDefault();
-                document.getElementById("easycredit_installment_agreement_error").style.display = "block";
-            } else {
-                paymentForm.submit();
-            }
+    if (paymentForm.elements['payment_easycreditinstallment'] && paymentForm.elements['payment_easycreditinstallment'].checked === true && document.getElementById("easycredit_installment_agreement_error")) {
+        document.getElementById("easycredit_installment_agreement_error").style.display = "none";
+        if (document.getElementById("easycredit_installment_agreement").checked === false) {
+            e.preventDefault();
+            document.getElementById("easycredit_installment_agreement_error").style.display = "block";
+        } else {
+            paymentForm.submit();
         }
+    } else {
+        paymentForm.submit();
     }
-    return true
 }
